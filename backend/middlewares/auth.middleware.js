7eca6e7947bee5c519de43452f72fd39dbf690e7
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-09-25 09:19:26 +0530

    fix: correct parameter name in isAuth middleware and remove debug log

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 
-export const isAuth = async (req,res,rext) => {
+export const isAuth = async (req,res,next) => {
     try {
         const token = req.cookies.token
         if(!token){
@@ -10,7 +10,7 @@ export const isAuth = async (req,res,rext) => {
         if(!decodeToken){
             return res.status(400).json({message:"token not verified"})
         }
-        console.log(decodeToken);
+        // console.log(decodeToken);
         
         req.userId = decodeToken.userId
         next();
