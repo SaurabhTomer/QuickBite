ed7759b38a5c9eed6b904c6d96d8d1443cd70f5e
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-09-25 08:43:06 +0530

    feat: add user route for retrieving current user information

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cors from'cors'
 import { connectDb } from './config/db.js';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.routes.js';
+import userRouter from './routes/user.route.js';
 
 const app = express();
 const port = process.env.PORT || 5000
@@ -17,6 +18,8 @@ app.use(cors({
 }))
 
 app.use("/api/auth" , authRouter)
+app.use("/api/user" , userRouter)
+
 
 
 
