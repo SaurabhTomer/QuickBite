b538a262f4e9467e73a4e7b1fb62cad0d8394ce1
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-11-06 10:02:17 +0530

    craeted forgot password ui

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -3,15 +3,20 @@ import { IoIosArrowRoundBack } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 
 function ForgotPassword() {
-  const [step, setStep] = useState(1);
+    const navigate = useNavigate();
+
+  const [step, setStep] = useState(1); //state for steps
+  //states for user entered fields
   const [email, setEmail] = useState("");
-  const navigate = useNavigate();
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [otp, setOtp] = useState("");
 
   return (
     <div className="flex items-center justify-center w-full min-h-screen p-4 bg-[#fff9f6]">
       {/* forgot password heading */}
       <div className="bg-white rounded-xl shadow-lg w-full max-w-md p-8">
-        <div className="flex items-center gap-4 mb-4">
+        <div className="flex items-center gap-4 mb-4 cursor-pointer">
           <IoIosArrowRoundBack
             className="text-[#ff4d2d] "
             size={30}
@@ -41,15 +46,93 @@ function ForgotPassword() {
                 value={email}
               />
             </div>
+            {/*  send otp button */}
+            <button
+              className={
+                "w-full font-semibold pointer-cursor text-center bg-[#ff4d2d] text-white hover:bg-[#e64323] py-2 rounded-lg "
+              }
+            >
+              Send OTP
+            </button>
           </div>
         )}
 
-        {/*  send otp button */}
-        <button
-          className={'w-full font-semibold pointer-cursor text-center bg-[#ff4d2d] text-white hover:bg-[#e64323] py-2 rounded-lg '}
-        >
-          Send OTP
-        </button>
+        {/* step 2 */}
+        {step == 2 && (
+          <div>
+            {/* enter otp */}
+            <div className="mb-6">
+              <label
+                htmlFor="OTP"
+                className="block text-gray-700  font-medium mb-1"
+              >
+                OTP
+              </label>
+              <input
+                type="text"
+                className="w-full  border border-gray-200 rounded-lg px-3 py-2 focus:outline-none"
+                placeholder="Enter OTP "
+                onChange={(e) => setOtp(e.target.value)}
+                value={otp}
+              />
+            </div>
+            {/*  verify button */}
+            <button
+              className={
+                "w-full font-semibold pointer-cursor text-center bg-[#ff4d2d] text-white hover:bg-[#e64323] py-2 rounded-lg "
+              }
+            >
+              Verify OTP
+            </button>
+          </div>
+        )}
+
+        {/* step 3 */}
+        {step == 3 && (
+          <div>
+            {/* new password */}
+            <div className="mb-6">
+              <label
+                htmlFor="newPassword"
+                className="block text-gray-700  font-medium mb-1"
+              >
+                New Password
+              </label>
+              <input
+                type="password"
+                className="w-full  border border-gray-200 rounded-lg px-3 py-2 focus:outline-none"
+                placeholder=" New Password "
+                onChange={(e) => setNewPassword(e.target.value)}
+                value={newPassword}
+              />
+            </div>
+
+            {/* confirm password */}
+            <div className="mb-6">
+              <label
+                htmlFor="confirmPassword"
+                className="block text-gray-700  font-medium mb-1"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                className="w-full  border border-gray-200 rounded-lg px-3 py-2 focus:outline-none"
+                placeholder=" Confirm Password "
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                value={confirmPassword}
+              />
+            </div>
+            {/*  password button */}
+            <button
+              className={
+                "w-full font-semibold pointer-cursor text-center bg-[#ff4d2d] text-white hover:bg-[#e64323] py-2 rounded-lg "
+              }
+            >
+              Reset Password
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
