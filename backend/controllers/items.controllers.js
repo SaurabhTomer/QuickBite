2d0f15a7b6d57383ac1b2a46b0546ef76d088da1
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-11-10 10:52:07 +0530

    items controllers updated

--- backend/controllers/items.controllers.js
+++ backend/controllers/items.controllers.js
@@ -6,13 +6,13 @@ export const addItem = async (req, res) => {
   try {
     //extract field
     const { name, category, foodType, price } = req.body;
-    //validate field 
+    //validate field
     if (!name || !category || !foodType || !price) {
-  return res.status(400).json({
-    success: false,
-    message: "All fields (name, category, foodType, price) are required.",
-  });
-}
+      return res.status(400).json({
+        success: false,
+        message: "All fields (name, category, foodType, price) are required.",
+      });
+    }
 
     let image = null;
     //this file is access by multer
@@ -43,12 +43,55 @@ export const addItem = async (req, res) => {
       .status(201)
       .json({ success: true, message: "Item created successfully", item });
   } catch (error) {
-    return res
-      .status(500)
-      .json({
+    return res.status(500).json({
+      success: false,
+      message: "addItem error ",
+      error: error.message,
+    });
+  }
+};
+
+export const editItem = async (req, res) => {
+  try {
+    //fetch item id which we want to change
+    const itemId = req.params.itemId;
+
+    //extract field
+    const { name, category, foodType, price } = req.body;
+    //validate field
+    if (!name || !category || !foodType || !price) {
+      return res.status(400).json({
         success: false,
-        message: "Item not created ",
-        error: error.message,
+        message: "All fields (name, category, foodType, price) are required.",
       });
+    }
+
+    let image = null;
+    //this file is access by multer
+    if (req.file) {
+      image = await uploadOnCloudinary(req.file.path);
+    }
+
+    const item = await Item.findByIdAndUpdate(
+      itemId,
+      { name, price, foodType, image, category },
+      { new: true }
+    );
+    if (!item) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Item not  found" });
+    }
+
+    //return res
+    return res
+      .status(201)
+      .json({ success: true, message: "Item upadted successfully", item });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "editItem error ",
+      error: error.message,
+    });
   }
 };
