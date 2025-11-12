9b637838ea8ad7720a5854b54cab5fb12fb5c976
Saurabh Tomer <tomersaurabh91@gmail.com>
2025-11-12 20:46:16 +0530

    added map slice in store

---
+++ frontend/src/redux/mapSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const mapSlice = createSlice({
+    name:"user",
+    // used to define particular thing inital state
+    initialState:{
+     
+
+    },
+    //reducres used to set initialState data
+    reducers:{
+        
+       
+    }
+})
+
+export const { } = mapSlice.actions;
+export default mapSlice.reducer
